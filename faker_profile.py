e9a6707f1671f643fa8b337fe56a1de4242479d0
Chukwujekwu <anahchukwujekwu@gmail.com>
2024-02-09 16:19:34 +0100

    files changed

--- faker_profile.py
+++ faker_profile.py
@@ -1,15 +1,15 @@
-import awswrangler as wr
-
-from utils.aws import aws_sesion
+from utils.aws import aws_session
 from utils.fakers_operations import profile_data_generator
+
+import awswrangler as wr
 
 
 def data_extract_to_s3():
     wr.s3.to_parquet(
-                    df=profile_data_generator(200),
-                    path="s3://faker-data-test-bucket/user_profile/",
-                    boto3_session=aws_sesion(),
-                    mode="append",
-                    dataset=True
-                    )
+        df=profile_data_generator(200),
+        path="s3://faker-data-test-bucket/user_profile/",
+        boto3_session=aws_session(),
+        mode="append",
+        dataset=True
+    )
     return "Data successfully written to s3"
